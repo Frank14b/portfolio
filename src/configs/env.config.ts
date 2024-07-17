922ccd5e784f0658e7ec9dcbb1ab7048ab2f7363
Frank14b <franckfontcha@gmail.com>
2024-07-17 04:59:05 +0400

    update contact form with validation and firebase add doc

--- src/configs/env.config.ts
+++ src/configs/env.config.ts
@@ -1,9 +1,19 @@
 export const ENV_CONFIG = {
-    SMTP: {
-        from: process.env.NEXT_PUBLIC_SMTP_FROM,
-        hostname: process.env.NEXT_PUBLIC_SMTP_HOST,
-        username: process.env.NEXT_PUBLIC_SMTP_USER,
-        password: process.env.NEXT_PUBLIC_SMTP_PASSWORD,
-        port: process.env.NEXT_PUBLIC_SMTP_PORT,
-    }
-}
+  SMTP: {
+    from: process.env.NEXT_PUBLIC_SMTP_FROM,
+    hostname: process.env.NEXT_PUBLIC_SMTP_HOST,
+    username: process.env.NEXT_PUBLIC_SMTP_USER,
+    password: process.env.NEXT_PUBLIC_SMTP_PASSWORD,
+    port: process.env.NEXT_PUBLIC_SMTP_PORT,
+  },
+  firebase: {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGE_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  },
+};
