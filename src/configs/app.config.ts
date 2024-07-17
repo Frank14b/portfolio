1dee352dca8ec50276388cfe1819047a6422f6a2
Frank14b <franckfontcha@gmail.com>
2024-07-17 08:47:55 +0400

    update contact form with validation and firebase add doc

--- src/configs/app.config.ts
+++ src/configs/app.config.ts
@@ -1,6 +1,18 @@
 import path from "path";
 
 export const EMAIL_FILE_URL = {
-  core: path.join(process.cwd(), "src", "emails", "email-core.html"),
-  contact: path.join(process.cwd(), "src", "emails", "welcome.html"),
+  core: path.join(
+    process.cwd(),
+    "public",
+    "8f04c1f1-507e-4e84-bf3e-df6b5e2f191e",
+    "emails",
+    "email-core.html"
+  ),
+  contact: path.join(
+    process.cwd(),
+    "public",
+    "8f04c1f1-507e-4e84-bf3e-df6b5e2f191e",
+    "emails",
+    "welcome.html"
+  ),
 };
